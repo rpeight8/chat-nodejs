080333cce6f60df3f197bfba5ac95c7e22be63f3
unknown <vitali.shuman96@gmail.com>
2023-08-02 16:14:29 +0300

    Add mock logic for Rooms handling

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
+import cors from "cors";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
-// const app = express();
-const server = http.createServer();
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
 const io = new Server(server, {
+  path: "/chat",
   cors: {
     origin: "http://localhost:5173",
   },
 });
 
+app.get("/api/rooms", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Room 1",
+    },
+    {
+      id: 2,
+      name: "Room 2",
+    },
+    {
+      id: 3,
+      name: "Room 3",
+    },
+  ]);
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.emit("hello", "world");
+  setTimeout(() => {
+    socket.emit("rooms:updated");
+  }, 3000);
 });
 
 server.listen(3000, () => {
